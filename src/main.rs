bbc5ea003895068cf77cd95600a32d8bf4c69191
tanacchi <q111026d@mail.kyutech.jp>
2020-12-31 16:38:12 +0900

    Make minimum web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App,
+                HttpResponse, HttpServer,
+                Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind("localhost:8080")?
+    .run()
+    .await
 }

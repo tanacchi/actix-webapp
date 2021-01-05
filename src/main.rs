105b21ce8ea3ba456572b58a6d319398a5d1075b
tanacchi <q111026d@mail.kyutech.jp>
2021-01-06 02:22:08 +0900

    Set config of database to App

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,17 @@ async fn main() -> std::io::Result<()> {
         HttpServer,
     };
     use crate::routes;
+    use dotenv::dotenv;
+    use tokio_postgres::NoTls;
+
+    dotenv().ok();
+
+    let config = config::Config::from_env().unwrap();
+    let pool = config.pg.create_pool(NoTls).unwrap();
 
     HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .configure(routes::app_config)
     })
     .bind("localhost:8080")?

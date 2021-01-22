a76699fa7ccd8daaff41037f47063e6d6df96c7a
tanacchi <q111026d@mail.kyutech.jp>
2021-01-22 16:55:24 +0900

    Add handlers and routes for user login / out and registeration

--- src/handlers.rs
+++ src/handlers.rs
@@ -22,21 +22,6 @@ pub async fn count(data: web::Data<state::AppStateWithCounter>) -> String {
     format!("Request number: {}", counter)
 }
 
-pub async fn form() -> Result<HttpResponse> {
-    Ok(HttpResponse::Ok()
-        .content_type("text/html; charset=utf-8")
-        .body(include_str!("../static/form.html")))
-}
-
-use crate::{db, models::User, error::MyError};
-use deadpool_postgres::{Client, Pool};
-pub async fn register(params : web::Form<param::ParamsForRegister>, db_pool: web::Data<Pool>) -> Result<HttpResponse> {
-    let user_info = User {name: params.name.clone()};
-    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
-    let new_user = db::add_user(&client, user_info).await?;
-    Ok(HttpResponse::Ok().json(new_user))
-}
-
 pub async fn user_list(db_pool: web::Data<Pool>) -> Result<HttpResponse> {
     let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
     let users = db::get_all_users(&client).await?;
@@ -66,4 +51,31 @@ pub async fn add_category(params: web::Form<param::ParamsForNewCategory>, db_poo
     let _new_category = Category {name: params.name.clone()};
     let new_category = db::add_category(&client, _new_category).await?;
     Ok(HttpResponse::Ok().json(new_category))
+}
+
+pub async fn signup_form() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../static/form.html")))
+}
+
+use crate::{db, models::User, error::MyError};
+use deadpool_postgres::{Client, Pool};
+pub async fn signup(params : web::Form<param::ParamsForRegister>, db_pool: web::Data<Pool>) -> Result<HttpResponse> {
+    let user_info = User {name: params.name.clone()};
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+    let new_user = db::add_user(&client, user_info).await?;
+    Ok(HttpResponse::Ok().json(new_user))
+}
+
+pub async fn signin_form() -> Result<HttpResponse> {
+    unimplemented!();
+}
+
+pub async fn signin() -> Result<HttpResponse> {
+    unimplemented!();
+}
+
+pub async fn signout() -> Result<HttpResponse> {
+    unimplemented!();
 }

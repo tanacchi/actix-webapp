19e40f6b88732329f8770bc0cd64964f2cce4550
tanacchi <q111026d@mail.kyutech.jp>
2021-01-22 15:17:49 +0900

    Fix method and tag-name on form

--- src/templates.rs
+++ src/templates.rs
@@ -3,9 +3,9 @@ use maud::html;
 pub fn category_form() -> String {
     let content = html! {
         p { "Hello" }
-        form {
+        form method="POST" {
             label for="comment" { "Input Some comment." }
-            input type="textarea" name="comment";
+            input type="textarea" name="name";
         }
     };
     content.into_string()

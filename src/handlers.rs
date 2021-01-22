f723ae8af25e6626569d33275cb88569cccf15c3
tanacchi <q111026d@mail.kyutech.jp>
2021-01-22 18:45:46 +0900

    Rename param structure for signup and signin

--- src/handlers.rs
+++ src/handlers.rs
@@ -66,7 +66,7 @@ pub async fn signup_form() -> Result<HttpResponse> {
 
 use crate::{db, models::User, error::MyError};
 use deadpool_postgres::{Client, Pool};
-pub async fn signup(params : web::Form<param::ParamsForRegister>, db_pool: web::Data<Pool>) -> Result<HttpResponse> {
+pub async fn signup(params : web::Form<param::ParamsForSignUp>, db_pool: web::Data<Pool>) -> Result<HttpResponse> {
     let user_info = User {name: params.name.clone()};
     let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
     let new_user = db::add_user(&client, user_info).await?;

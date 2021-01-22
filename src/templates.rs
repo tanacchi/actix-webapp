1cffd079894ad0ba0e03e30290cf0db033c5d4c9
tanacchi <q111026d@mail.kyutech.jp>
2021-01-22 09:46:02 +0900

    Make category form with maud

--- src/templates.rs
+++ src/templates.rs
@@ -1,5 +1,12 @@
 use maud::html;
 
 pub fn category_form() -> String {
-    "ahi--".to_string()
+    let content = html! {
+        p { "Hello" }
+        form {
+            label for="comment" { "Input Some comment." }
+            input type="textarea" name="comment";
+        }
+    };
+    content.into_string()
 }

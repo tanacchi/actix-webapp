a4243d4ce53d5b552c183582ec2f0daca3e2d2ef
tanacchi <q111026d@mail.kyutech.jp>
2021-01-22 22:26:26 +0900

    Register logger on service

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ async fn main() -> std::io::Result<()> {
     use actix_web::{
         App,
         HttpServer,
+        middleware::Logger,
     };
     use crate::routes;
     use dotenv::dotenv;
@@ -26,6 +27,7 @@ async fn main() -> std::io::Result<()> {
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .data(pool.clone())
             .configure(routes::app_config)
 

eb1a632c7f956e3a2bf48b461524e83ea70c189f
tanacchi <q111026d@mail.kyutech.jp>
2021-01-24 02:50:01 +0900

    Add reports model

--- src/models.rs
+++ src/models.rs
@@ -12,3 +12,12 @@ pub struct User {
 pub struct Category {
     pub name: String,
 }
+
+#[derive(Deserialize, PostgresMapper, Serialize)]
+#[pg_mapper(table = "reports")]
+pub struct Reports {
+    pub comment: String,
+    pub date: String,
+    category_id: i64,
+    user_id: i64
+}

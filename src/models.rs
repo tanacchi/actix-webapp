8bad710621f91c2a7ba4024cb5b08911660c5346
tanacchi <q111026d@mail.kyutech.jp>
2021-01-25 15:50:56 +0900

    Add id property to all models of data

--- src/models.rs
+++ src/models.rs
@@ -4,18 +4,21 @@ use tokio_pg_mapper_derive::PostgresMapper;
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "users")]
 pub struct User {
+    pub id: i64,
     pub name: String,
 }
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "categories")]
 pub struct Category {
+    pub id: i64,
     pub name: String,
 }
 
 #[derive(Deserialize, PostgresMapper, Serialize)]
 #[pg_mapper(table = "reports")]
 pub struct Report {
+    pub id: i64,
     pub comment: String,
     pub date: String,
     pub category_id: i64,
